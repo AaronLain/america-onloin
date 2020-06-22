ff4ec4c5d0a4a1f43c6e0fed31e26242e713f1ef
Aaron Lain <aaron.j.lain@gmail.com>
2020-06-22 18:50:46 -0500

    added a few things

---
+++ src/components/pages/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Auth Container!</h1>
+        <h4 className="subtitle">Please login to see your stuff</h4>
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/components/shared/TheNavBar/TheNavBar.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import firebase from 'firebase/app'
+import 'firebase/auth';
+
+import './TheNavBar.scss';
+
+class TheNavBar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  logOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    
+    const buildTheNavBar = () => {
+      const { authed } = this.props;
+      if (authed) {
+        return (
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/home'>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/new'>New Item</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/favorites'>New Item</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink onClick={this.logOut}>Logout</NavLink>
+            </NavItem>
+          </Nav>
+        );
+      }
+      return (
+        <Nav className="ml-auto" navbar>
+          <button className="btn btn-warning ml-auto" onClick={this.loginClickEvent}>Google Login</button>
+        </Nav>
+      );
+    };
+
+    return (
+      <div className="DatNavbar">
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            {buildTheNavBar()}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  } 
+}
+
+export default TheNavBar;
